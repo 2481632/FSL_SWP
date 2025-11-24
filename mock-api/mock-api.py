9402b07e63f2db19fd4a33773e893bc009560590
alpef04 <alpef04@mi.fu-berlin.de>
2025-11-24 13:34:04 +0100

    mock api

---
+++ mock-api/mock-api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import json
+import os
+
+app = Flask(__name__)
+
+file_path = os.path.join(os.path.dirname(__file__), "example_station.json")
+with open(file_path, "r", encoding="utf-8") as f:
+    JSON_DATA = json.load(f)
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    return jsonify(JSON_DATA)
+
+if __name__ == "__main__":
+    app.run(debug=True)
